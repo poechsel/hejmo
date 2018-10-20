7064d35b5581264874ba092325e601e4f87c72a3
poechsel <pierre.oechsel@gmail.com>
2018-10-20 18:32:23 +0200

    use of foursquare api

--- backend/foursquare_api.py
+++ backend/foursquare_api.py
@@ -2,28 +2,36 @@
 ### Foursquare is useful to extract categories from locations.
 
 import json, requests
+import foursquare
+import auth.foursquare_secret as secret
 
 categories_list_url = 'https://api.foursquare.com/v2/venues/categories'
 venue_from_coordinates_url = 'https://api.foursquare.com/v2/venues/search'
 
-app_id = "5OFFKXRXZNT1T2QURFRTEJGDLQ2UAUZRG2XVYAMDOIMNQEYL"
-app_secret = "14OHZJOYESUQODBWU32V4NJYUM0UHO0IU4HZZW4F453LYJYI"
+
+client = foursquare.Foursquare(
+    client_id=secret.id,
+    client_secret=secret.secret,
+    redirect_uri='http://fondu.com/oauth/authorize')
+auth_uri = client.oauth.auth_url()
 
 def get_venue_ID(latitude, longitude):
     params = dict(
         client_id=app_id,
         client_secret=app_secret,
-        ll='',
+        ll=','.join([str(latitude), str(longitude)]),
         intent='browse',
-        radius='10',
+        radius='250',
         llAcc='100'
     )
+    return client.venues.search(params)
+    
 
 def get_venue_details(venue_ID):
-    params = dict(
-        client_id=app_id,
-        client_secret=app_secret,
-    )
+    return client.venues(venue_ID)
 
 
 def get_venue_category(venue_ID):
+    pass
+
+# print(get_venue_details("412d2800f964a520df0c1fe3"))
