aed6ea7b0e0ac31b4cdc5b2b9a924681dabc7ca1
Lucas Pluvinage <lucas.pluvinage@gmail.com>
2018-10-20 23:47:47 +0200

    Long-term storage structure and bindings

--- backend/database.py
+++ backend/database.py
@@ -1,1 +1,81 @@
 ### This file will manage users and aggregate data.
+from numpy import genfromtxt
+import json
+import foursquare_api
+import os
+
+locations_to_rate_file = "locations_to_rate.csv"
+profile_file = "profile.csv"
+ratings_file = "ratings.csv"
+
+userlist_file = "data/users/userlist.json"
+
+venue_data_file = "info.json"
+
+
+# list of [place_id, name, date_of_visit, photo_url, description, location]
+def get_user_locations_to_rate(user_id):
+    return genfromtxt("data/"+str(user_id)+"/"+locations_to_rate_file, delimiter=',')
+
+# list of [category_id, rating, confidence]
+def get_user_profile(user_id):
+    return genfromtxt("data/"+str(user_id)+"/"+profile_file, delimiter=',')
+
+# list of [place_id, rating, confidence, tov => [time_of_visits]]
+def get_user_ratings(user_id):
+    return genfromtxt("data/"+str(user_id)+"/"+ratings_file, delimiter=',')
+
+def get_userlist():
+    with open(userlist_file) as file:
+        return json.load(file)
+
+# dict user -> user_profile
+def get_profiles():
+    users = get_userlist()
+    result = []
+    for user in users:
+        result.append(get_user_profile(user))
+    return result
+
+# dict user -> user_ratings
+def get_ratings():
+    users = get_userlist()
+    result = []
+    for user in users:
+        result.append(get_user_ratings(user))
+    return result
+
+# list of [place_id, name, photo, description, longitude, latitude, [categories]]
+def get_venue_data(venue_ID):
+    try:
+        with open("data/venues/"+str(venue_ID)+"/"+venue_data_file, "r") as read_file:
+            data = json.load(read_file)
+            return data
+    except OSError as _: # venue info isn't cached yet
+        # gather data from foursquare.
+        details = foursquare_api.get_venue_details(venue_ID)
+        photo_url = details["bestPhoto"]["prefix"] + "300x500" + details["bestPhoto"]["suffix"]
+        
+        categories_list = []
+        for cat in details["categories"]:
+            categories_list.append(cat["id"])
+        
+        info = {
+            "name": details["name"],
+            "photo": photo_url,
+            "description": details["description"],
+            "longitude": details["location"]["lng"],
+            "latitude": details["location"]["lat"],
+            "categories": categories_list
+        }
+
+        write_venue_data(venue_ID, info)
+
+
+def write_venue_data(venue_ID, venue_data):
+    filename = "data/venues/"+str(venue_ID)+"/"+venue_data_file
+    if not os.path.exists(os.path.dirname(filename)):
+        os.makedirs(os.path.dirname(filename))
+
+    with open(filename, "w") as write_file:
+        json.dump(venue_data, write_file)
--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,52 @@
 from flask import Flask
 app = Flask(__name__)
+import json
+import database
 
 @app.route('/')
 def hello_world():
     return ''
 
-"""
+'''
 Get vector profile of an user.
 {
     feature_vector: array[float];
 }
-"""
+'''
 @app.route('/get_profile/<int:user_id>')
 def get_profile(user_id):
+    pass
 
-"""
+
+
+'''
+Get a list of locations for the user to rate.
+Returns
+{
+    Name
+    Date of visit
+    Photo
+    Description
+    Location
+}
+'''
+@app.route('/get_locations_to_rate/<int:user_id>')
+def get_locations_to_rate(user_id):
+    user_data = database.get_locations_to_rate(user_id)
+    output = []
+    for entry in user_data:
+        output.append({
+            "place_id": entry[0],
+            "name": entry[1],
+            "date_of_visit": entry[2],
+            "photo_url": entry[3],
+            "description": entry[4],
+            "location": entry[5]
+        })
+    return json.dumps(output)
+
+
+'''
 Get rated locations of an user.
 {
     place_id: int;
@@ -23,11 +55,13 @@ Get rated locations of an user.
     time_of_visit_mean: int;
     time_of_visit_spread: float;
 }
-"""
+'''
 @app.route('/get_locations/<int:user_id>')
 def get_locations(user_id):
+    pass
 
-"""
+
+'''
 Returns recommendations for an user.
 {
     recommendations: array[{
@@ -39,20 +73,21 @@ Returns recommendations for an user.
         tip: string;
     }]
 }
-"""
+'''
 @app.route('/get_recommendations/<int:user_id>/<int:category>/<float:location_x>/<float:location_y>/<int:time>/')
 def get_recommendations(user_id, category, location_x, location_y, time):
-    
+    pass
 
-"""
+'''
 Register a location with a rating, time of visit and an optional POST comment.
-"""
+'''
 @app.route('/put_location/<int:user_id>/<int:place_id>/<float:rating>/<int:time_of_visit>/')
 def put_location(user_id, place_id, rating, time_of_visit):
+    pass
 
-
-"""
+'''
 Set category affinity.
-"""
+'''
 @app.route('/put_affinity/<int:user_id>/<int:category>/<float:affinity>/')
 def put_affinity(user_id, category, affinity):
+    pass
